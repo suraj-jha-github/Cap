87d2d54d024ea7df9f00efba24a1b5eb3ae0ac8c
808302 <mejhasuraj@gmail.com>
2022-08-12 18:55:37 +0530

    first commit

---
+++ src/components/screens/Home.js
@@ -0,0 +1,279 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { UserContext } from "../../App";
+// import UserProfile from "./UserProfile";
+// import ThumbUpAltOutlinedIcon from '@material-ui/icons/ThumbUpAltOutlined';
+// import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+
+const Home = () => {
+
+  const [data, setData] = useState([]);
+  const { state, dispatch } = useContext(UserContext);
+  useEffect(() => {
+    fetch("/allpost", {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        // console.log(result);
+        setData(result.posts);
+      });
+  }, []);
+  const likePost = (id) => {
+    fetch("/like", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+      body: JSON.stringify({
+        postId: id,
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        //   console.log(result)
+        const newData = data.map((item) => {
+          if (item._id === result._id) {
+            return result;
+          } else {
+            return item;
+          }
+        });
+        setData(newData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const heartPost = (id) => {
+    fetch("/heart", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + localStorage.getItem("jwt"),
+      },
+      body: JSON.stringify({
+        postId: id,
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+          // console.log(result)
+        const newData = data.map((item) => {
+          if (item._id === result._id) {
+            return result;
+          } else {
+            return item;
+          }
+        });
+        setData(newData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const unheartPost = (id) => {
+    fetch("/unheart", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+      body: JSON.stringify({
+        postId: id,
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        //   console.log(result)
+        const newData = data.map((item) => {
+          if (item._id === result._id) {
+            return result;
+          } else {
+            return item;
+          }
+        });
+        setData(newData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const unlikePost = (id) => {
+    fetch("/unlike", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+      body: JSON.stringify({
+        postId: id,
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        //   console.log(result)
+        const newData = data.map((item) => {
+          if (item._id === result._id) {
+            return result;
+          } else {
+            return item;
+          }
+        });
+        setData(newData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const makeComment = (text, postId) => {
+    fetch("/comment", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+      body: JSON.stringify({
+        postId,
+        text,
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        // console.log(result);
+        const newData = data.map((item) => {
+          if (item._id === result._id) {
+            return result;
+          } else {
+            return item;
+          }
+        });
+        setData(newData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const deletePost = (postid) => {
+    fetch(`/deletepost/${postid}`, {
+      method: "delete",
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        // console.log(result);
+        const newData=data.filter(item=>{
+            return item._id !==result._id
+        })
+        setData(newData)
+      });
+  };
+
+  return (
+    <div className="home">
+      {data.map((item) => {
+        return (
+          <div className="card home-card" key={item._id}>
+            <h5>
+              <Link to={item.postedBy._id !== state._id?"/profile/"+item.postedBy._id :"/profile/"} className="posted-user" >{item.postedBy.name} </Link>
+              
+              {item.postedBy._id === state._id && (
+                <i className="material-icons"  style={{ color: "black",float:"right",marginRight: "30px",
+                marginTop: "8px"}}
+                onClick={()=>deletePost(item._id)}
+                >
+                  delete
+                </i>
+              )}
+            </h5>
+            <div className="card-image">
+              <img src={item.photo} alt="" />
+            </div>
+            <div className="card-content">
+              <div style={{display:"flex"}}>
+              {item.heart.includes(state._id)?
+              <i className="material-icons" 
+              onClick={()=>{unheartPost(item._id)}}
+              style={{ color: "red"}}>
+                favorite
+              </i>
+              :
+              <i className="material-icons"
+              style={{color:"red"}}
+              onClick={()=>{heartPost(item._id)}}
+              >
+               favorite_border
+             </i>
+              
+              }
+              
+              
+              {item.likes.includes(state._id) ? (
+                <i
+                  className="material-icons"
+                  onClick={() => {
+                    unlikePost(item._id);
+                  }}
+                >
+                  thumb_down 
+                 
+                </i>
+              ) : (
+                <i
+                  className="material-icons"
+                  onClick={() => {
+                    likePost(item._id);
+                  }}
+                  
+                >
+                  thumb_up
+                  {/* <ThumbUpAltOutlinedIcon/> */}
+                  
+                </i>
+              )}
+              </div>
+              <div style={{display:"flex"}}>
+              <h6 style={{marginRight:"1rem"}}>{item.heart.length} hearts </h6>
+              <h6>{item.likes.length} likes </h6>
+              </div>
+              
+              <h4>{item.title}</h4>
+              <p>{item.body}</p>
+              {item.comments.map((record) => {
+                return (
+                  <h6 key={record._id}>
+                    <span style={{ fontWeight: "500" }}>
+                      {record.postedBy.name}{" "}
+                    </span>
+                    {record.text}{" "}
+                  </h6>
+                );
+              })}
+              <form
+             
+                onSubmit={(e) => {
+                  
+                  e.preventDefault();
+                  makeComment(e.target[0].value, item._id);
+                }}
+              >
+                
+                <input type="text" placeholder="add a comment" />
+                
+              </form>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Home;
